6b4a1d64e5ea49ad9edbcd72b5b1b0e01a0f7f4c
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-05-19 09:28:35 +0800

    Added MYSQL Connections 👌

--- src/database_function/MYSQLiteConnection.java
+++ src/database_function/MYSQLiteConnection.java
@@ -9,5 +9,26 @@ package database_function;
  * @author Administrator
  */
 public class MYSQLiteConnection {
-    
+     static String databaseUrl = "jdbc:mysql://localhost:3306/hoteldatabase";
+    static String user ="root";
+    static String password = "";
+
+
+    void intializeConnection() {
+        try {
+            System.out.println("Loading MySQL driver...");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded successfully.");
+            java.sql.Connection connection = java.sql.DriverManager.getConnection(databaseUrl, user, password);
+            System.out.println("Connection established successfully");
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Error establishing connection: " + e.getMessage());
+            e.printStackTrace(); // Print stack trace for debugging
+        }
+    }
+    public  static  void main(String[] args) {
+        MYSQLiteConnection connection = new MYSQLiteConnection();
+        connection.intializeConnection();
+    }
 }
